f9540e099f551de4cc378690d3a0b48d8226a314
DaanKorver <daank2000@hotmail.nl>
2022-06-01 20:45:45 +0200

    basic chat setup

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,33 @@
 const socket = io()
+const chat = document.querySelector('.chatroom form')
+const chatInput = document.querySelector('.chatroom form input')
+const chats = document.querySelector('.chats')
+
+chat.addEventListener('submit', event => {
+	event.preventDefault()
+	const msg = chatInput.value
+	if (!msg) return
+	socket.emit('message', msg)
+	chatInput.value = ''
+})
+
+/* ------------------------ */
+/* Socket.io                */
+/* ------------------------ */
+
+// Chat messages
+socket.on('message', msg => {
+	const direction = msg.id === socket.id ? 'outgoing' : 'incoming'
+	chats.insertAdjacentHTML(
+		'beforeend',
+		`
+    <li class="${direction}">
+      <div class="user">
+        <img src="/assets/images/joost-chippr.jpg" alt="Joost">
+      </div>
+      <p>${msg.msg}</p>
+    </li>
+  `
+	)
+	chats.scrollTo(0, chats.scrollHeight)
+})
